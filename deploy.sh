a3e8f390588cc82ba6172f366e699ca1dceacaca
NCIGareth <x19122748@student.ncirl.ie>
2023-03-23 18:46:24 +0000

    SQLiteIssues

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,6 @@ sudo npm install -g pm2
 pm2 stop InventoryCRUD
 cd InventoryCRUD/
 npm install
+npm uninstall sqlite3
+npm install --save sqlite3
 pm2 start server.js --name InventoryCRUD --env=production
