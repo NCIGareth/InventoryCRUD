69d9beaf19b402d0d20fe6077805f17dccd91464
unknown <1902badrovvedran@gmail.com>
2023-04-06 16:52:04 +0100

    Deploy.sh Privatekey & Server

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ sudo npm install -g pm2
 pm2 stop InventoryCRUD
 cd InventoryCRUD/
 npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
 npm uninstall sqlite3
 npm install --save sqlite3
 pm2 start server.js --name InventoryCRUD --env=production
